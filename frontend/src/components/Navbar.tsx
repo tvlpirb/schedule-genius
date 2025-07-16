6aa7a929dae8a26934bfd1c832e6d105c06db5c4
Talhah Peerbhai <talhah.peerbhai@gmail.com>
2025-07-16 21:43:11 +0300

    Add skeleton navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+
+  return (
+    <>
+      <nav className="navbar shadow-sm glass min-h-3 z-30 fixed top-0">
+        <div className="navbar-start">
+          <h1 className="font-bold">Schedule Genius</h1>
+        </div>
+        <div className="navbar-center">
+
+        </div>
+        <div className="navbar-end">
+          <ul className="ml-auto">
+            <li>Test</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
