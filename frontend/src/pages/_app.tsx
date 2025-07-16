7adf753b6917b5535ce4920bb39b626c2d81c02b
Talhah Peerbhai <talhah.peerbhai@gmail.com>
2025-07-16 21:42:39 +0300

    Add initial page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,20 @@
+import { GlobalProviders } from "@/context/GlobalProviders";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <Head>
+        <title>Schedule Genius</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <GlobalProviders>
+        <Component {...pageProps} />
+      </GlobalProviders>
+    </>
+  );
 }
+
